2d89d8e86c7b1185a5fed1f1da5c1ea82d2eb872
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-01-30 17:47:13 -0300

    Style menu mobile

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const hamburger = document.querySelector('#hamburger');
-let nav = document.querySelector('.navbar > ul');
+let nav = document.querySelector('.navbar > ul ');
+const portfolio = document.getElementById('portfolio-nav')
+const about = document.getElementById('about-nav')
+const contact = document.getElementById('contact-nav')
+
+
 
 hamburger.addEventListener('click', function(){
- nav.style.display = 'flex';
- nav.style.flexDirection = 'column';
- hamburger.style.display = 'none';
- document.querySelector('.menuicon').style.display = 'none';
+    nav.style.display = 'flex';
+    nav.style.flexDirection = 'column';
+    nav.style.alignItems = 'left';
+    nav.style.margin = '28px';
+    nav.style.listStyle = 'none';
+    hamburger.style.display = 'none';
+    document.querySelector('.menuicon').style.display = 'none';
+    portfolio.style.textDecoration = 'none';
+    about.style.textDecoration = 'none';
+    contact.style.textDecoration = 'none';
+    nav.style.gap = '40px';
 });
