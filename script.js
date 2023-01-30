2d8a5bb552b9c354eb278ecd64ac73dd7b7ad463
Muhammad <umedjon02.ud@gmail.com>
2023-01-31 02:18:02 +0500

    Finish the mobile menu showup

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let nav = document.querySelector('.navbar > ul ');
 const portfolio = document.getElementById('portfolio-nav')
 const about = document.getElementById('about-nav')
 const contact = document.getElementById('contact-nav')
-
+const closeBtn = document.querySelector('#close-btn');
+const toolbar = document.querySelector('.toolbar');
 
 
 hamburger.addEventListener('click', function(){
@@ -12,10 +13,34 @@ hamburger.addEventListener('click', function(){
     nav.style.alignItems = 'left';
     nav.style.margin = '28px';
     nav.style.listStyle = 'none';
-    hamburger.style.display = 'none';
-    document.querySelector('.menuicon').style.display = 'none';
+    toolbar.style.display = 'none';
     portfolio.style.textDecoration = 'none';
     about.style.textDecoration = 'none';
     contact.style.textDecoration = 'none';
     nav.style.gap = '40px';
+    closeBtn.hidden= false;
+    closeBtn.style.float='right';
+    closeBtn.style.marginRight = '40px';
 });
+
+closeBtn.addEventListener('click', function(){
+    nav.style.display ='none';
+    closeBtn.style.display = 'none';
+    toolbar.style.display = 'flex';
+})
+
+portfolio.addEventListener('click', function(){
+    nav.style.display ='none';
+    closeBtn.style.display = 'none';
+    toolbar.style.display = 'flex';
+})
+about.addEventListener('click', function(){
+    nav.style.display ='none';
+    closeBtn.style.display = 'none';
+    toolbar.style.display = 'flex';
+})
+contact.addEventListener('click', function(){
+    nav.style.display ='none';
+    closeBtn.style.display = 'none';
+    toolbar.style.display = 'flex';
+})
